9051f036d0c1a7d1200ac41858d09c0eec7fda74
jhonatan182 <jona.vand98@gmail.com>
2025-12-25 20:19:21 -0600

    feat: dtos, entity products

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,1 +1,19 @@
-export class CreateProductDto {}
+import { Type } from 'class-transformer';
+import { IsNumber, IsString, Min } from 'class-validator';
+
+export class CreateProductDto {
+  @IsString({ message: 'El nombre es obligatorio' })
+  public name: string;
+
+  @IsNumber(
+    {
+      maxDecimalPlaces: 4,
+    },
+    {
+      message: 'Formato de precio incorrecto',
+    },
+  )
+  @Min(0, { message: 'El precio debe ser mayor o igual a 0' })
+  @Type(() => Number)
+  public price: string;
+}
